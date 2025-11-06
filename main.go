b3c88b5a254da546923aceab974c571d2bc884c3
Dmytro Varhan <vorjin6272@gmail.com>
2025-11-06 21:14:11 +0200

    add default html

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"html/template"
+	"net/http"
 	"os"
+	"strings"
 )
 
 type Option struct {
@@ -34,5 +37,31 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(storyFile["intro"])
+	fmt.Println("Starting the server on :8080")
+	err = http.ListenAndServe(":8080", storyHandler(storyFile))
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func storyHandler(str map[string]Chapter) http.HandlerFunc {
+	tmpl := template.Must(template.ParseFiles("html/story.html"))
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := strings.Trim(r.URL.Path, "/")
+
+		if story, ok := str[path]; ok {
+			fmt.Println(story)
+			//executing template
+			err := tmpl.Execute(w, nil)
+			if err != nil {
+				panic(err)
+			}
+
+			return
+		} else {
+			fmt.Printf("Chapter key not found: %s", r.URL.Path)
+		}
+	}
 }
